8bde9e544c21c3a4473436d8ac732b9ddc5e989d
Phuong Tran <phuongtt1@imt-soft.com>
2023-12-27 20:11:44 +0700

    copy2

---
+++ Online-Store/OnlineStore.BusinessLogic/Dtos/Product/ProductInCartDto - Copy.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineStore.BusinessLogic.Dtos.Product
+{
+    public class ProductInCartDto
+    {
+        public int ProductId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
